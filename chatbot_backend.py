ef4a83c1f643364a25faf46395fbf9a662e00724
Michael Ramsay <96756649+m-a-yasmar@users.noreply.github.com>
2023-11-15 21:22:42 +0000

    Update chatbot_backend.py

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -141,7 +141,6 @@ def ask():
     query_vector = vectorizer.transform([query])
     
     if session.get('returning_user', True):
-        session['conversation_status'] = 'active'
         session['conversation'] = [
             {"role": "system", "content": "You are a friendly professional medical receptionist. Your primary responsibilities include collecting patient information, responding to queries with compassion, and helping them arrange appointments with suitable healthcare professionals."}
         ]
@@ -150,11 +149,9 @@ def ask():
     elif session.get('returning_user', False):
         welcome_message = "Hello and a warm welcome! I'm Sam, your AI medical receptionist here to assist you. Before we proceed may I have your full name please?"
         session['conversation'].append({"role": "assistant", "content": welcome_message})
-        session['conversation_status'] = 'active'
         session.modified = True #
         return jsonify({"answer": welcome_message})
    
-    #else session.get('conversation_status', 'active') == 'active':
     else:
         custom_prompt = {
             "role": "system",
